7c8a50990e046067c54a443feab809b59ddf8e82
muza <muzallyefah@gmail.com>
2023-11-12 19:39:57 +0800

    feat: create pdf loader

---
+++ src/lib/pdf-loader.ts
@@ -0,0 +1,20 @@
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+export async function getPdfData(path: string) {
+  try {
+    const loader = new PDFLoader(path, { splitPages: false });
+    const docs = await loader.load();
+
+    // const textSplitter = new RecursiveCharacterTextSplitter({
+    //   chunkSize: 1000,
+    //   chunkOverlap: 200,
+    // });
+
+    // const chunkedDocs = await textSplitter.splitDocuments(docs);
+
+    return docs;
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
+}
