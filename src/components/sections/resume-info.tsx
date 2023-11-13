70f1e5e6a0ea810e97e56a9ba922f3609e0f6c2b
muza <muzallyefah@gmail.com>
2023-11-13 13:09:59 +0800

    feat: add edit button

--- src/components/sections/resume-info.tsx
+++ src/components/sections/resume-info.tsx
@@ -1,5 +1,7 @@
 import SectionContainer from "@/components/section-container"
 import { Resume } from "@/lib/models/resume"
+import { Button } from "../ui/button";
+import { EditIcon } from "lucide-react";
 
 export default function ResumeInfo({ resume }: { resume: Resume | null }) {
   const getEducation = () => resume && resume.education ? resume.education.map((e) => (
@@ -101,6 +103,12 @@ export default function ResumeInfo({ resume }: { resume: Resume | null }) {
   return (
     //todo: fill resume info and make it editable
     <SectionContainer title={ "Resume Info" } description={ "Double-check the Details to Make Sure Everything is Accurate" } isOpen={ resume !== null }>
+      <div className="flex justify-end">
+            <Button variant='secondary' className="w-full sm:w-auto">
+              <EditIcon className="mr-2 h-4 w-4" />
+              Edit
+            </Button>
+        </div>
       <ul>
         <li>
           <h4 className="text-sm font-medium">Personal Info</h4>
