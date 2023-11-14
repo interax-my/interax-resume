21d98bc87c3f800a158401a0963c7492e635d25d
ainnaziha <ainnaziha551@gmail.com>
2023-11-14 23:06:02 +0800

    feat: skill edit

--- src/components/sections/edit-skill.tsx
+++ src/components/sections/edit-skill.tsx
@@ -12,34 +12,41 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Resume } from "@/lib/models/resume"
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons"
 import { EditIcon } from "lucide-react"
 import { useState } from "react"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/seperator"
 
 export function EditSkill({ resume, setResume }: { resume: Resume, setResume: (info: Resume) => void }) {
     const [open, setOpen] = useState(false);
-    const [name, setName] = useState(resume.personalInfo?.name ?? '');
-    const [occupation, setOccupation] = useState(resume.personalInfo?.occupation ?? '');
-    const [experience, setExperience] = useState(resume.personalInfo?.experience ?? '');
-    const [email, setEmail] = useState(resume.personalInfo?.email ?? '');
-    const [location, setLocation] = useState(resume.personalInfo?.location ?? '');
+    const [skill, setSkill] = useState<string[]>(resume.skills ?? []);
 
     const onSubmit = (event: { preventDefault: () => void }) => {
         event.preventDefault();
         const editedResume: Resume = {
             ...resume,
-            personalInfo: {
-                name: name,
-                occupation: occupation,
-                experience: Number(experience),
-                location: location,
-                email: email,
-            }
+            skills: skill,
         };
 
         setResume(editedResume);    
         setOpen(false);
     };
 
+
+    const add = () => {
+        const item = [
+            ...skill,
+            ''
+        ];
+        setSkill(item);
+     };
+     
+    const remove = (index: number) => {
+        const item = skill.filter((_, i) => i !== index);
+        setSkill(item);
+    };
+     
     return (
     <Dialog open={open} onOpenChange={setOpen} >
         <DialogTrigger asChild>
@@ -52,66 +59,36 @@ export function EditSkill({ resume, setResume }: { resume: Resume, setResume: (i
             <DialogHeader>
                 <DialogTitle>Edit Info</DialogTitle>
             </DialogHeader>
-            <div className="grid gap-4 py-4">
-                <h4 className="text-sm font-medium text-primary">Personal Info</h4>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                    Name
-                    </Label>
-                    <Input
-                    id="name"
-                    value={ name }
-                    onChange={(e) => setName(e.target.value)}
-                    className="col-span-3"
-                    />
+            <ScrollArea className="h-full px-3">
+                <div className="grid gap-5 py-4">
+                <div className="grid grid-cols-5 gap-4 items-center">
+                    <h4 className="text-sm font-medium text-primary col-span-4">Skills</h4>
+                    <div className="col-span-1 flex justify-end">
+                        <Button variant="outline" size="icon" onClick={ add }>
+                            <PlusIcon className="h-4 w-4" />
+                        </Button>
+                    </div>
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="occupation" className="text-right">
-                    Occupation
-                    </Label>
-                    <Input
-                    id="occupation"
-                    value={ occupation }
-                    onChange={(e) => setOccupation(e.target.value)}
-                    className="col-span-3"
-                    />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="experience" className="text-right">
-                    Experience
-                    </Label>
-                    <Input
-                    id="experience"
-                    value={ experience }
-                    onChange={(e) => setExperience(e.target.value)}
-                    type="number"
-                    className="col-span-3"
-                    />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="email" className="text-right">
-                    Email
-                    </Label>
-                    <Input
-                    id="email"
-                    value={ email }
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    className="col-span-3"
-                    />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="location" className="text-right">
-                    Location
-                    </Label>
-                    <Input
-                    id="location"
-                    value={ location }
-                    onChange={(e) => setLocation(e.target.value)}
-                    className="col-span-3"
-                    />
-                </div>
+                { skill.map((e, index) => (
+                    <div key={ `edit-${index}` } className="grid grid-cols-5 gap-4 items-center mb-2">
+                        <Input
+                        value={ e ?? '' }
+                        onChange={(e) => {
+                            const edited = [...skill];
+                            edited[index] = e.target.value;
+                            setSkill(edited);
+                        }}
+                        className="col-span-4"
+                        />
+                        <div className="col-span-1 flex justify-end">
+                            <Button variant="outline" size="icon" onClick={() => remove(index)}>
+                                <MinusIcon className="h-4 w-4" />
+                            </Button>
+                        </div>
+                    </div>
+                ) )}
             </div>
+            </ScrollArea>
             <DialogFooter>
                 <Button type="submit" onClick={ onSubmit }>Save changes</Button>
             </DialogFooter>
