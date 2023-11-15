519c6bb103474a74c0229b21501e58c18f9d05ce
Aqilzikry Arman <aqilzikry97@gmail.com>
2023-11-15 15:52:00 +0800

    Add util function for parsing JSON from Cohere API

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const tryParseJson = (str: string) => {
+  try {
+    const regex = /\,(?!\s*?[\{\[\"\'\w])/g;
+
+    let txt = str.replaceAll('```json', '');
+    txt = txt.replaceAll('```', '');
+    txt.replace(regex, '')
+    return JSON.parse(txt);
+  } catch (e) {
+    console.log('Error parsing JSON');
+  }
+};
