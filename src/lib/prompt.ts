0fa4dabe572fcb4b9eb3d69fd8961c83c5f9edba
Aqilzikry Arman <aqilzikry97@gmail.com>
2023-11-18 20:54:09 +0800

    Reduce likelihood of 1. JSON object in strings 2. Formatting suggestions

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -121,17 +121,20 @@ export const generateExtractPdfPrompt = (content: string) => {
 
 export const generateImproveResumePrompt = (content: string) => {
   return `
-  As a career consultant, you will evaluate the following tasks to enhance the resume. Respond with a JSON object {"grammar_error": [], "content_optimization": [], "suitable_roles": []}. Insert only strings in the JSON arrays and not an object. Strings should be in sentence format and not list format. All strings should be self-contained and does not need external context.
+  As a career consultant, you will evaluate the following tasks to enhance the resume. Respond with a JSON object {"grammar_error": [], "content_optimization": [], "suitable_roles": []}. Insert only strings in the JSON arrays and not an object. Strings should be in sentence format and not list format. All strings should be self-contained and does not need external context. All strings should be in human-readable format.
 
   NOTE: If any information is just an empty string or value, consider it as invalid.
 
   ### Task List ###
 
-  **Grammar Check**
+  **Grammar Error**
   Review the content for grammatical errors. Provide corrections and suggestions to ensure a polished, error-free document. Offer corrections or suggestions to enhance its structure. The content is in a JSON format, only review the JSON values.
 
   **Content Optimization**
-  Analyze the resume content to enhance its performance in Applicant Tracking Systems (ATS). Ensure that relevant keywords, skills, and experiences are appropriately emphasized to maximize the likelihood of successfully passing through applicant tracking systems. The resume content should effectively reflect the person's target job position. Provide specific suggestions or optimizations based on ATS best practices.
+  Analyze the resume content to enhance its performance in Applicant Tracking Systems (ATS). Ensure that relevant keywords, skills, and experiences are appropriately emphasized to maximize the likelihood of successfully passing through applicant tracking systems. The resume content should effectively reflect the person's target job position. Provide specific suggestions or optimizations based on ATS best practices. 
+  NOTES:
+  1. Suggestions with 'bullet points', 'format', and 'JSON' keywords are to be removed.
+  2. Suggestions should be directly applicable to the resume.
   
   **Suitable Roles**
   Identify potential roles based on the resume. Offer suggestions for roles that align with the individual's skills, experiences, and qualifications. For each suggestion, describe your thought process using format "roles: suggestion".
